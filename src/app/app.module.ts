f3ee7ad16371593c02b699734d3761fad3217e71
Jason Allen <Jason94Allen@hotmail.com>
2023-12-08 12:34:34 -0500

    Initial commit before updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PersonAddComponent } from './person-add/person-add.component';
 import { PersonDeleteComponent } from './person-delete/person-delete.component';
 import { TaxFormParentComponent } from './tax-form-parent/tax-form-parent.component';
 import { TaxPersoninfoChildComponent } from './tax-personinfo-child/tax-personinfo-child.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, PersonPageComponent, PersonEditComponent, PersonAddComponent, PersonDeleteComponent, TaxFormParentComponent, TaxPersoninfoChildComponent],
@@ -22,6 +23,7 @@ import { TaxPersoninfoChildComponent } from './tax-personinfo-child/tax-personin
     HttpClientModule,
     ReactiveFormsModule,
     FontAwesomeModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
